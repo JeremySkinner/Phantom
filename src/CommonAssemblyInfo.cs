d5a2b8f3a1e257a84fada57a73cd316e5a6d2c87
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2009-08-08 10:42:51 +0100

    Initial structure

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,30 @@
+#region License
+
+// Copyright 2009 Jeremy Skinner (http://www.jeremyskinner.co.uk)
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//  
+// http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// 
+// The latest version of this file can be found at http://github.com/JeremySkinner/Spectre
+
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Spectre.Core")]
+[assembly: AssemblyDescription("Spectre")]
+[assembly: AssemblyProduct("Spectre")]
+[assembly: AssemblyCopyright("Copyright (c) Jeremy Skinner 2009")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

9e6f8731d369dd0326be85bc2a2ad8f535a5764a
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2009-12-29 19:11:39 +0000

    Add CommonAssemblyInfo.cs to nant project.

---
+++ src/Phantom.Integration.NAnt/CommonAssemblyInfo.cs
@@ -0,0 +1,28 @@
+#region License
+
+// Copyright Jeremy Skinner (http://www.jeremyskinner.co.uk)
+// 
+// Licensed under the Microsoft Public License. You may
+// obtain a copy of the license at:
+// 
+// http://www.microsoft.com/opensource/licenses.mspx
+// 
+// By using this source code in any fashion, you are agreeing
+// to be bound by the terms of the Microsoft Public License.
+// 
+// You must not remove this notice, or any other, from this software.
+// 
+// The latest version of this file can be found at http://github.com/JeremySkinner/Phantom
+
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Phantom")]
+[assembly: AssemblyDescription("Phantom")]
+[assembly: AssemblyProduct("Phantom")]
+[assembly: AssemblyCopyright("Copyright (c) Jeremy Skinner 2009")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

8715e6b3d68a065bd12461dde0cc64ccb3279281
Marcus Andersson <systemvetaren@gmail.com>
2011-07-08 16:51:46 +0200

    Added class to holt mstest builtin.

---
+++ src/Phantom.Core/Builtins/MSTest.cs
@@ -0,0 +1,9 @@
+﻿namespace Phantom.Core.Builtins
+{
+	public class mstest : ExecutableTool<mstest>
+	{
+		protected override void Execute() {
+			throw new System.NotImplementedException();
+		}
+	}
+}

d2f8d33bbe74a71cd5cbeda2bf80cca06dfa1dce
Marcus Andersson <systemvetaren@gmail.com>
2011-07-11 12:55:04 +0200

    Class to hold MSTest integration.

--- src/Phantom.Core/Builtins/MSTest.cs
+++ src/Phantom.Core/Builtins/MSTest.cs
@@ -1,9 +1,20 @@
-﻿namespace Phantom.Core.Builtins
-{
-	public class mstest : ExecutableTool<mstest>
-	{
+﻿namespace Phantom.Core.Builtins {
+	using System.Collections.Generic;
+
+	public class mstest : ExecutableTool<mstest> {
+		readonly List<string> toolPaths;
+
+		public mstest() {
+			toolPaths.Add(@"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\mstest.exe");
+			toolPaths.Add(@"C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\mstest.exe");
+			toolPaths.Add(@"C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\mstest.exe");
+			toolPaths.Add(@"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\mstest.exe");
+		}
+
 		protected override void Execute() {
-			throw new System.NotImplementedException();
+			if(string.IsNullOrWhiteSpace(toolPath)) {
+				
+			}
 		}
 	}
 }

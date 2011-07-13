d3668cb73599908dc2e266e97c26134683def764
Marcus Andersson <systemvetaren@gmail.com>
2011-07-13 17:33:06 +0200

    Added license info..

--- src/Phantom.Core/Builtins/MSTest.cs
+++ src/Phantom.Core/Builtins/MSTest.cs
@@ -1,4 +1,20 @@
-﻿namespace Phantom.Core.Builtins {
+﻿#region License
+
+// Copyright Jeremy Skinner (http://www.jeremyskinner.co.uk) and Contributors
+// 
+// Licensed under the Microsoft Public License. You may
+// obtain a copy of the license at:
+// 
+// http://www.microsoft.com/opensource/licenses.mspx
+// 
+// By using this source code in any fashion, you are agreeing
+// to be bound by the terms of the Microsoft Public License.
+// 
+// You must not remove this notice, or any other, from this software.
+
+#endregion
+
+namespace Phantom.Core.Builtins {
 	using System.Collections.Generic;
 	using System.IO;
 	using System.Linq;

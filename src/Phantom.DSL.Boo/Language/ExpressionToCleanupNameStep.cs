fb0e92e8e5e69ec516c9be0676450512ecedc857
Ken Egozi <mail@kenegozi.com>
2012-06-14 23:32:10 -0700

    script cleanup support

---
+++ src/Phantom.DSL.Boo/Language/ExpressionToCleanupNameStep.cs
@@ -0,0 +1,38 @@
+#region License
+
+// Copyright Jeremy Skinner (http://www.jeremyskinner.co.uk) and Contributors
+// 
+// Licensed under the Microsoft Public License. You may
+// obtain a copy of the license at:
+// 
+// http://www.microsoft.com/opensource/licenses.mspx
+// 
+// By using this source code in any fashion, you are agreeing
+// to be bound by the terms of the Microsoft Public License.
+// 
+// You must not remove this notice, or any other, from this software.
+
+#endregion
+
+namespace Phantom.Core.Language {
+	using Boo.Lang.Compiler.Ast;
+	using Boo.Lang.Compiler.Steps;
+
+	public class ExpressionToCleanupNameStep : AbstractTransformerCompilerStep {
+		public override void Run() {
+			Visit(CompileUnit);
+		}
+
+		public override void OnMemberReferenceExpression(MemberReferenceExpression node) {
+			if (PhantomDslEngine.IsCleanupMethod(node.ParentNode)) {
+				ReplaceCurrentNode(new StringLiteralExpression(node.ToCodeString()));
+			}
+		}
+
+		public override void OnReferenceExpression(ReferenceExpression node) {
+			if (PhantomDslEngine.IsCleanupMethod(node.ParentNode)) {
+				ReplaceCurrentNode(new StringLiteralExpression(node.Name));
+			}
+		}
+	}
+}
